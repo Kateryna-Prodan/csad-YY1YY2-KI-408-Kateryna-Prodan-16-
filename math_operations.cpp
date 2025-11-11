ac953752dbaa73ceec2fa55cf59f1c8a3a2444d5
Kateryna-Prodan <kateryna.prodan.ki.2022@lpnu.ua>
2025-11-11 21:22:29 +0200

    UnitTestsTask3

---
+++ math_operations.cpp
@@ -0,0 +1,5 @@
+#include "math_operations.h"
+// Function definition
+int add(int a, int b) {
+    return a + b;
+}
---
+++ math_operations.h
@@ -0,0 +1,7 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+// Function declaration
+int add(int a, int b);
+
+#endif // MATH_OPERATIONS_H
