c229e41fb41739833a903b1d4709e94a820f53ba
Kateryna-Prodan <kateryna.prodan.ki.2022@lpnu.ua>
2025-11-26 15:23:20 +0200

    Remove unnecessary files from PR for stage 4

--- math_operations.cpp
+++
@@ -1,5 +0,0 @@
-#include "math_operations.h"
-// Function definition
-int add(int a, int b) {
-    return a + b;
-}
--- math_operations.h
+++
@@ -1,7 +0,0 @@
-#ifndef MATH_OPERATIONS_H
-#define MATH_OPERATIONS_H
-
-// Function declaration
-int add(int a, int b);
-
-#endif // MATH_OPERATIONS_H
